b261839f4aa0f208ee1f73ed5514825bf05e1f4c
Mudrak <mudrakpatel96@gmail.com>
2017-01-18 11:30:38 -0500

    Completed question 2

--- comp311_Week01/Program.cs
+++ comp311_Week01/Program.cs
@@ -11,18 +11,18 @@ namespace comp311_Week01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int a = 1, b = 2, c;
-            //runtime errors
-            // c = a / b;
-            c = add(a, b);
-            Console.WriteLine("c = 1 + 2 = " + c);
+            int a = 1, b = 0, c;
+            ////runtime errors
+            c = a / b;
+            //c = add(a, b);
+            //Console.WriteLine("c = 1 + 2 = " + c);
         }
         /**
          @method add
          @return => int
          @params => a : int, b : int
          */
-         static int add(int a,int b)
+        static int add(int a,int b)
         {
             return a + b;
         }
