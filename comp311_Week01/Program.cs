f1c4dda59a4c324cb8d71f79d62879e0e982f67e
Mudrak <mudrakpatel96@gmail.com>
2017-01-18 11:16:00 -0500

    Added code to practice testing

--- comp311_Week01/Program.cs
+++ comp311_Week01/Program.cs
@@ -10,6 +10,21 @@ namespace comp311_Week01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            int a = 1, b = 2, c;
+            //runtime errors
+            // c = a / b;
+            c = add(a, b);
+            Console.WriteLine("c = 1 + 2 = " + c);
+        }
+        /**
+         @method add
+         @return => int
+         @params => a : int, b : int
+         */
+         static int add(int a,int b)
+        {
+            return a + b;
         }
     }
 }
